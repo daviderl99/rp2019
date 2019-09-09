88e891d503ff4ec8466c3c984c8dd1deb7a877a1
daviderl99 <daviderlich7@hot.ee>
2019-09-09 14:43:37 +0300

    Added item to item page

--- static/item-script.js
+++
@@ -1,5 +0,0 @@
-const x = window.location;
-let urlParams = new URLSearchParams(x.search);
-const title = urlParams.get("title");
-const cost = urlParams.get("cost");
-const src = urlParams.get("src");
---
+++ static/item.js
@@ -0,0 +1,32 @@
+const x = window.location;
+let urlParams = new URLSearchParams(x.search);
+const title = urlParams.get("title");
+const cost = urlParams.get("cost");
+const src = urlParams.get("src");
+
+window.onload = function(){
+    createItem(title, cost, src);
+}
+
+createItem = (title, cost, src) => {
+    let content_div = document.createElement("DIV");
+    let item_div = document.createElement("DIV");
+    let title_div = document.createElement("DIV");
+    let cost_div = document.createElement("DIV");
+    let img = document.createElement("IMG");
+
+    content_div.className = "content";
+    item_div.className = "item";
+    title_div.className = "item_title"
+    cost_div.className = "item_cost";
+
+    img.src = src;
+    title_div.innerHTML = title;
+    cost_div.innerHTML = cost + " EUR";
+
+    item_div.appendChild(img);
+    item_div.appendChild(title_div);
+    item_div.appendChild(cost_div);
+    content_div.appendChild(item_div);
+    document.body.appendChild(content_div);
+}
