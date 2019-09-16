6c50010b8ae36799464d110926e69f4a1d50f41b
daviderl99 <daviderlich7@hot.ee>
2019-09-16 13:13:42 +0300

    Added item page back

--- static/scripts/utils.js
+++ static/scripts/utils.js
@@ -1,16 +1,17 @@
 function createItemElement(item){
     const anchor = document.createElement("A");
-    anchor.href = "";
     const itemContainer = document.createElement("DIV");
     itemContainer.className = "item";
-
+    
     const imgElement = document.createElement("IMG");
     imgElement.src = item.imgSrc;
 
+    anchor.href = "./item.html?title=" + item.title + "&price="+ item.price + "&src=" + imgElement.src;
+    
     const priceElement = document.createElement("DIV");
     priceElement.innerText = "€ " + item.price;
     priceElement.className = "item_price";
-
+    
     const titleElement = document.createElement("DIV");
     titleElement.className = "item_title";
     titleElement.textContent = item.title;
