69895192754cada556966ba78a2e5307db2d26a0
daviderl99 <daviderlich7@hot.ee>
2019-09-16 13:05:26 +0300

    utils.js

--- static/scripts/itemsList.js
+++ static/scripts/itemsList.js
@@ -6,44 +6,17 @@ window.onload = function(){
     }, false);
 }
 
-function createItems(type){
+function createItems(item){
     const root = document.getElementById("content");
     root.innerHTML = "";
-    if (type == "phone"){
-        phones.forEach((phone)=>{
-            const element = createItemElement(phone);
-            root.append(element);
-        });
+    if (item == "phone"){
+        items = phones;
     }
-    else if (type == "laptop") {
-        laptops.forEach((laptop)=>{
-            const element = createItemElement(laptop);
-            root.append(element);
-        });
+    else if (item == "laptop"){
+        items = laptops;
     }
-}
-
-function createItemElement(item){
-    const anchor = document.createElement("A");
-    anchor.href = "";
-    const itemContainer = document.createElement("DIV");
-    itemContainer.className = "item";
-
-    const imgElement = document.createElement("IMG");
-    imgElement.src = item.imgSrc;
-
-    const priceElement = document.createElement("DIV");
-    priceElement.innerText = "€ " + item.price;
-    priceElement.className = "item_price";
-
-    const titleElement = document.createElement("DIV");
-    titleElement.className = "item_title";
-    titleElement.textContent = item.title;
-
-    anchor.append(itemContainer);
-    itemContainer.append(imgElement);
-    itemContainer.append(titleElement);
-    itemContainer.append(priceElement);
-
-    return anchor;
+    items.forEach((item)=>{
+        const element = createItemElement(item);
+        root.append(element);
+    });
 }
---
+++ static/scripts/utils.js
@@ -0,0 +1,24 @@
+function createItemElement(item){
+    const anchor = document.createElement("A");
+    anchor.href = "";
+    const itemContainer = document.createElement("DIV");
+    itemContainer.className = "item";
+
+    const imgElement = document.createElement("IMG");
+    imgElement.src = item.imgSrc;
+
+    const priceElement = document.createElement("DIV");
+    priceElement.innerText = "€ " + item.price;
+    priceElement.className = "item_price";
+
+    const titleElement = document.createElement("DIV");
+    titleElement.className = "item_title";
+    titleElement.textContent = item.title;
+
+    anchor.append(itemContainer);
+    itemContainer.append(imgElement);
+    itemContainer.append(titleElement);
+    itemContainer.append(priceElement);
+
+    return anchor;
+}
