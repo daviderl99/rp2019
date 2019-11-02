1dd97897f94962b3ee0da255412219ca01a376fc
daviderl99 <daviderlich7@hot.ee>
2019-11-02 18:19:58 +0200

    Lesson 5: MongoDB Getting Started

--- server/server.js
+++ server/server.js
@@ -3,6 +3,37 @@ const app = express()
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const DB = require("./database.js");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
+var kittySchema = new mongoose.Schema({
+    name: String
+});
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+const kitten1 = new Kitten({
+    name: "red cat"
+});
+
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0-szs3x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+mongoose.connect(DB_URL)
+    .then(() => {
+        console.log("DB access successful");
+        kitten1.save(err => {
+            if (err){
+                console.error("kitten error");
+            }
+            else{
+                console.log("Successful save");
+            }
+        });
+    })
+    .catch(err => {
+        console.error("error", err);
+    });
 
 // GET all items
 app.get("/api/items", (req, res)=>{
